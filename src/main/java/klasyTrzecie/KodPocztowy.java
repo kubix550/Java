b0a070a54a6b995308841c63b246691be1cc34ef
Kuba Kwiecinski <jkwiecinski550@gmail.com>
2020-05-17 15:14:03 +0200

    added KodPocztowy

---
+++ src/main/java/klasyTrzecie/KodPocztowy.java
@@ -0,0 +1,31 @@
+package klasyTrzecie;
+
+// 2.Napisz program, który sprawdza czy podany wyraz jest poprawnym kodem pocztowym
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class KodPocztowy {
+    public static void main(String[] args) {
+        String text;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Wpisz kod pocztowy:");
+        text = input.nextLine();
+
+        // okreslenie obiektu typu pattern, na jego podstawie jest podobienstwo
+        // wyrazenie \\d oznacza cyfre, nawias typu {liczba} okresla ile razy poprzedzajace wyrazenie ma wystapic
+        // trzeba uzywac podwojnych znakow \\ jezeli pattern jest w quotation mark
+        Pattern pattern = Pattern.compile("\\d{2}-\\d{3}");
+
+        // okreslenie obiektu typu matcher, szuka podobenstwa we wpisanym tekscie
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            System.out.println("Kod pocztowy jest poprawny");
+        } else {
+            System.out.println("Niepoprawny kod pocztowy");
+        }
+    }
+}
