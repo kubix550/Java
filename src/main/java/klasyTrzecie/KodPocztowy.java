da19825af3277306eccbcb3bac0524fe5c10fc66
Kuba Kwiecinski <jkwiecinski550@gmail.com>
2020-05-23 13:21:55 +0200

    updated KodPocztowy

--- src/main/java/klasyTrzecie/KodPocztowy.java
+++ src/main/java/klasyTrzecie/KodPocztowy.java
@@ -17,6 +17,7 @@ public class KodPocztowy {
         // okreslenie obiektu typu pattern, na jego podstawie jest podobienstwo
         // wyrazenie \\d oznacza cyfre, nawias typu {liczba} okresla ile razy poprzedzajace wyrazenie ma wystapic
         // trzeba uzywac podwojnych znakow \\ jezeli pattern jest w quotation mark
+        // zamiast \\d mozna zrobic [0-9]
         Pattern pattern = Pattern.compile("\\d{2}-\\d{3}");
 
         // okreslenie obiektu typu matcher, szuka podobenstwa we wpisanym tekscie
@@ -27,5 +28,8 @@ public class KodPocztowy {
         } else {
             System.out.println("Niepoprawny kod pocztowy");
         }
+
+        // mozna krocej
+        System.out.println(Pattern.matches("\\d{2}-\\d{3}", text));
     }
 }
