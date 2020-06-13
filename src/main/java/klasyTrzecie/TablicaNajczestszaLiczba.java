2d4b29118e98e7082da3d5165ba502cd1c04a279
Kuba Kwiecinski <jkwiecinski550@gmail.com>
2020-06-13 14:23:13 +0200

    file refactor

--- src/main/java/klasyTrzecie/TablicaNajczestszaLiczba.java
+++ src/main/java/klasyTrzecie/TablicaNajczestszaLiczba.java
@@ -2,60 +2,30 @@ package klasyTrzecie;
 
 // 4.W tablicy jednowymiarowej znajdź liczbę, która występuje najczęściej.
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
-
 public class TablicaNajczestszaLiczba {
     public static void main(String[] args) {
-        int[] tablica = {1, 5, 25, -10, 15, 2, 2, 1, 7, 6, 6, 7, 2, 2, 2, 2, 6, 9, 4, 2, 0};
-        int maxKlucz = tablica[0];
-        int maxValue = 1;
 
-        // mapa przechowuje pary (klucz, wartosc) np (25,1)
-        Map<Integer,Integer> mapa = new TreeMap<>();                    // TreeMap sortuje po kluczach od razu podczas wstawiania
+        // w tablicy nie moga byc liczby ujemne ze wzgledu na sposob rozwiazania
+        // druga tablica nie moze byc indeksowana liczbami ujemnymi
+        int[] tablica = {1, 5, 25, 10, 15, 2, 2, 1, 7, 6, 6, 7, 2, 2, 2, 2, 6, 9, 4, 2, 0};
+        int najczestszaLiczba = 0;
+
+        int[] tab = new int[26];
+
+        // domyslnie podstawia kazdemu z tab wartosc 0, bo nie wiadomo ile jest kazdego elementu
+        for (int i = 0; i < tab.length ; i++) {
+            tab[i] = 0;
+        }
 
         for (int i = 0; i < tablica.length ; i++) {
-            if (mapa.containsKey(tablica[i]))  {                        // sprawdza czy istnieje juz taki klucz
-                mapa.replace(tablica[i], mapa.get(tablica[i]) + 1);     // mapa.replace jezeli znajdzie drugi raz wartosc, a mapa.get pobiera nowa wartosc
-            }
-            else mapa.put(tablica[i], 1);                            // podstawia 1 jezeli element z tablicy wystapi pierwszy raz
+            // tablica[i] to elementy do zapisania w tablicy tab
+            tab[tablica[i]] = tab[tablica[i]] + 1;
         }
 
-        // dokladne wyswietlanie elementow mapy
-        // entryset wyswietla jednoczesnie klucz i wartosc
-
-//        for (Map.Entry<Integer, Integer> para: mapa.entrySet()) {
-//            System.out.println();
-//            System.out.println(para.getKey() + " - klucz");
-//            System.out.println(para.getValue() + " - wartosc");
-//        }
-
-        // szybsze wyswietlanie
-        System.out.println(mapa);
-
-        for (Map.Entry<Integer, Integer> para: mapa.entrySet()){
-            if (para.getValue() > maxValue) {
-                maxKlucz = para.getKey();
-                maxValue = para.getValue();
-            }
+        for (int i = 0; i < tab.length ; i++) {
+            System.out.println("Indeks tab:" + i);
+            System.out.println("Wartosc tab:" + tab[i]);
+            System.out.println();
         }
-
-        System.out.println("Liczba " + maxKlucz + " wystepuje najczesciej: " + maxValue + " razy");
-
-
-        // todo przerobic drugi sposob tylko na tablicy
-//        int[] tablica = {1, 5, 25, -10, 15, 2, 2, 1, 7, 6, 6, 7, 2, 2, 2, 2, 6, 9, 4, 2, 0};
-//        int najczestszaLiczba = 0;
-//
-//        Arrays.sort(tablica);
-//
-//        for (int i = 0; i < tablica.length; i++) {
-//            if (tablica[i] == tablica[i + 1]) {
-//                System.out.println(tablica[i]);
-//            }
-//        }
-
     }
 }
