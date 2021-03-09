a9e21577693b05e9a86b2475976c68bd1bfbe59d
Kuba Kwiecinski <jkwiecinski550@gmail.com>
2021-03-09 23:22:01 +0100

    added metodaTrapezow in CalkowanieNumeryczne

--- src/main/java/pozostale/CalkowanieNumeryczne.java
+++ src/main/java/pozostale/CalkowanieNumeryczne.java
@@ -1,11 +1,13 @@
 package pozostale;
 
+import java.util.Scanner;
+
 public class CalkowanieNumeryczne {
 
     // globalne zmienne (a, b - granice, n - ilosc prostokatow)
     static double a = 0;
     static double b = 1;
-    static double n = 100000000;
+    static double n = 10000;
 
     public static double f(double x) {
         // bo funkcja liniowa
@@ -24,11 +26,25 @@ public class CalkowanieNumeryczne {
         return suma;
     }
 
+    public static double metodaTrapezow() {
+        double suma = 0;
+        double szerokosc = (b-a)/n;
+
+        for (int i = 0; i < n - 1 ; i++) {
+            // oblicza sume wszystkich podstaw trapezow (kreski pionowe)
+            suma = suma + f(a + i * szerokosc);
+        }
+
+        suma = (suma + (f(a) + f(b)) / 2) * szerokosc;
+
+        return suma;
+    }
+
     public static void main(String[] args) {
         System.out.println(metodaProstokatow());
+        System.out.println(metodaTrapezow());
 
         // todo
         // dokladnosc - np. 0.001, to policzy az osiagnie to od prawdziwej wartosci
-        // dopisac metode z trapezami
     }
 }
